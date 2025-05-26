db300c16dda615927cf3d339c8ddd8e4ec1a1024
Salvador Torres-Montilla <salvatm18@gmail.com>
2025-05-26 16:02:59 +0200

    Esto son los cambios en DESCRIPTION, NAMESPACE y demás como consecuencia

--- R/RNAseqEasy.R
+++ R/RNAseqEasy.R
@@ -4,63 +4,63 @@ library(devtools)
 library(testthat)
 library(available)
 
-usethis::use_git()
-
-usethis::use_github()
-
-use_r("topGO_All")
-use_r("load_topGO_db")
-use_r("RNAseqEasy")
-
-use_mit_license()
-
-library(tidyverse)
-
-Mpo_GO_GOSLIM <- read_delim("G:/Mi unidad/Antiguo Drive/CNB/Marchantia/v6.1r1/Blazquez GO_db_no1.csv",
-                            delim = "\t", escape_double = FALSE,
-                            trim_ws = TRUE, col_names = TRUE)
-
-use_data(Mpo_GO_GOSLIM)
-
-use_rmarkdown_template("topGO_All")
-
-use_vignette(name = "topGO_All", title = "Simplify topGO Enrichment and plotting")
-
-
-## List of other packages to import when using this package
-use_package("dplyr", type = "Imports")
-use_package("tidyr", type = "Imports")
-use_package("ggplot2", type = "Imports")
-use_package("readr", type = "Imports")
-use_package("tximport", type = "Imports")
-use_package("DESeq2", type = "Imports")
-use_package("pheatmap", type = "Imports")
-use_package("topGO", type = "Imports")
-use_package("WGCNA", type = "Imports")
-use_package("GO.db", type = "Imports")
-use_package("rrvgo", type = "Imports")
-use_package("ggsci", type = "Imports")
-use_package("Hmisc", type = "Imports")
-use_package("openxlsx", type = "Imports")
-
-
-
-library(tximport)
-library(DESeq2)
-library(tidyverse)
-library(phylotools)
-library(pheatmap)
-library(topGO)
-library(WGCNA)
-library(Hmisc)
-library(openxlsx)
-library(wordcloud2)
-library(GO.db)
-library(htmlwidgets)
-library(rrvgo)
-library(pals)
-library(reshape2)
-library(ggsci)
-library(factoextra)
-
-
+# use_r("topGO_All")
+# use_r("load_topGO_db")
+# use_r("RNAseqEasy")
+#
+# use_mit_license()
+#
+# use_testthat()
+#
+# use_test(name = "topGO_All")
+# test_file("../tests/testthat/test-topGO_All.R")
+# library(tidyverse)
+#
+# Mpo_GO_GOSLIM <- read_delim("G:/Mi unidad/Antiguo Drive/CNB/Marchantia/v6.1r1/Blazquez GO_db_no1.csv",
+#                             delim = "\t", escape_double = FALSE,
+#                             trim_ws = TRUE, col_names = TRUE)
+#
+# use_data(Mpo_GO_GOSLIM, overwrite = TRUE)
+#
+# use_rmarkdown_template("topGO_All")
+#
+# use_vignette(name = "topGO_All", title = "Simplify topGO Enrichment and plotting")
+#
+#
+# ## List of other packages to import when using this package
+# use_package("dplyr", type = "Imports")
+# use_package("tidyr", type = "Imports")
+# use_package("ggplot2", type = "Imports")
+# use_package("readr", type = "Imports")
+# use_package("tximport", type = "Imports")
+# use_package("DESeq2", type = "Imports")
+# use_package("pheatmap", type = "Imports")
+# use_package("topGO", type = "Imports")
+# use_package("WGCNA", type = "Imports")
+# use_package("GO.db", type = "Imports")
+# use_package("rrvgo", type = "Imports")
+# use_package("ggsci", type = "Imports")
+# use_package("Hmisc", type = "Imports")
+# use_package("openxlsx", type = "Imports")
+#
+#
+#
+# library(tximport)
+# library(DESeq2)
+# library(tidyverse)
+# library(phylotools)
+# library(pheatmap)
+# library(topGO)
+# library(WGCNA)
+# library(Hmisc)
+# library(openxlsx)
+# library(wordcloud2)
+# library(GO.db)
+# library(htmlwidgets)
+# library(rrvgo)
+# library(pals)
+# library(reshape2)
+# library(ggsci)
+# library(factoextra)
+#
+#
