e77462ecffbed23d2366efcc2ec50ba250b3f326
Salvador Torres-Montilla <salvatm18@gmail.com>
2025-06-17 17:42:49 +0200

    Uso split en vez del for loop tras comprobar su mejor rendimiento con microbenchmark

--- R/load_topGO_db.R
+++ R/load_topGO_db.R
@@ -18,27 +18,12 @@
 load_topGO_db <- function(Annotation_file, GeneID, GOterms){
   # First, we rename columns to make it easier
   Annotation_file %>% dplyr::rename(ID = GeneID,
-                             GO = GOterms)
+                                    GO = GOterms)
   ## We remove duplicated rows
   Annotation_file <- Annotation_file[!duplicated(Annotation_file),]
   ## Transform to data frame
   Annotation_file <- as.data.frame(Annotation_file)
-  ## We create an empty list
-  GO_list <- list()
-  ## For loop row by row, assigning GeneIDs and GO terms to objects
-  for (row in seq(1, nrow(Annotation_file))){
-    ID <- Annotation_file[row,1]
-    GO <- Annotation_file[row,2]
-    ## GeneIDs will be names of the lists and GOterms the content. If a GeneID was already included, the new GO terms will be appended
-    if (ID %in% names(GO_list)){
-      GO_list[[ID]] <- append(GO_list[[ID]], GO)
-    } else {
-      GO_list[ID] <- GO
-    }
-
-  }
-  geneID2GO <- GO_list
+  geneID2GO <- split(Annotation_file[,GOterms], Annotation_file[,GeneID])
   return(geneID2GO)
 }
-
 
